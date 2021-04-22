fdf9e8f60ab85b1a060e856e05523a88392f8181
jthanawat <kla13thanawat@gmail.com>
2021-04-22 17:01:48 +0700

    ready for deployment

---
+++ comps/Navbar.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" width={40} height={20} />
+        <h1>Users List</h1>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/users">
+        <a>Users List</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,29 @@
-import Link from 'next/link';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Thanawat List | Home</title>
+        <title>Thanawat List | About</title>
         <meta name="keywords" content="thanawat" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
-        <Link href="/thanawat">
-          <a className={styles.btn}>See Thanawat Listing</a>
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
